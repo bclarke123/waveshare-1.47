38170a320a73c292b71fceb9915b7da569b7c51a
Ben Clarke <ben@arrayofstars.com>
2025-06-09 22:33:23 -0400

    Add boot button listener, clean up ws2812, cycle through images

--- examples/src/LCD_1in47_LVGL_test.c
+++ examples/src/LCD_1in47_LVGL_test.c
@@ -29,6 +29,10 @@
 ******************************************************************************/
 #include "LCD_Test.h"
 #include "LCD_1in47.h"
+#include "hardware/gpio.h"
+#include "hardware/sync.h"
+#include "hardware/structs/ioqspi.h"
+#include "hardware/structs/sio.h"
 #include "hardware/pio.h"
 #include "ws2812.pio.h"
  
@@ -38,23 +42,42 @@ static uint16_t LCD_1IN47_Read_KEY(void);
 
 #define IS_RGBW false
 #define NUM_PIXELS 1
-
-#ifdef PICO_DEFAULT_WS2812_PIN
-#define WS2812_PIN PICO_DEFAULT_WS2812_PIN
-#else
-// default to pin 2 if the board doesn't have a default WS2812 pin defined
 #define WS2812_PIN 22
-#endif
+
+bool __no_inline_not_in_flash_func(get_bootsel_button)() {
+    const uint CS_PIN_INDEX = 1;
+
+    // Must disable interrupts, as interrupt handlers may be in flash, and we
+    // are about to temporarily disable flash access!
+    uint32_t flags = save_and_disable_interrupts();
+
+    // Set chip select to Hi-Z
+    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+                    GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+    // Note we can't call into any sleep functions in flash right now
+    for (volatile int i = 0; i < 1000; ++i);
+
+    // The HI GPIO registers in SIO can observe and control the 6 QSPI pins.
+    // Note the button pulls the pin *low* when pressed.
+    #define CS_BIT SIO_GPIO_HI_IN_QSPI_CSN_BITS
+
+    bool button_state = !(sio_hw->gpio_hi_in & CS_BIT);
+
+    // Need to restore the state of chip select, else we are going to have a
+    // bad time when we return to code in flash!
+    hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+                    GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+                    IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+    restore_interrupts(flags);
+
+    return button_state;
+}
  
 static inline void put_pixel(PIO pio, uint sm, uint32_t pixel_grb) {
     pio_sm_put_blocking(pio, sm, pixel_grb << 8u);
-}
-
-static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b) {
-    return
-            ((uint32_t) (r) << 8) |
-            ((uint32_t) (g) << 16) |
-            (uint32_t) (b);
 }
 
 static inline uint8_t hue_to_rgb(uint16_t temp1, uint16_t temp2, uint16_t hue) {
@@ -118,12 +141,14 @@ static inline uint32_t hsl2rgb(uint8_t h, uint8_t s, uint8_t l) {
     }
 
     // Pack the RGB values into a uint32_t with the specified bit shifts
-    return ((uint32_t)r << 8) | ((uint32_t)g << 16) | (uint32_t)b;
+    return ((uint32_t)g << 8) | ((uint32_t)r << 16) | (uint32_t)b;
 }
 
 void pattern(PIO pio, uint sm, uint len, uint t) {
+    uint32_t pixel_color = hsl2rgb(t, 0xff, 0x7f);
+
     for (uint i = 0; i < len; ++i) {
-        put_pixel(pio, sm, hsl2rgb(t, 0xff, 0x7f));
+        put_pixel(pio, sm, pixel_color);
     }
 }
 
@@ -170,6 +195,9 @@ int LCD_1in47_test(void)
     Widgets_Init(dat);
 
     int t = 0;
+    int screen_index = 0;
+    bool is_pressed = false;
+    bool was_pressed = false;
 
     while(1)
     {
@@ -178,6 +206,16 @@ int LCD_1in47_test(void)
         t++;
         if (t % 256 == 0) {
             t = 0;
+        }
+
+        is_pressed = get_bootsel_button();
+        if (is_pressed && !was_pressed) {
+            was_pressed = true;
+            screen_index = (screen_index + 1) % 3;
+            lv_scr_load(dat->scr[screen_index]);
+        } else
+        if (was_pressed && !is_pressed) {
+            was_pressed = false;
         }
 
         lv_task_handler();
@@ -202,14 +240,25 @@ static void Widgets_Init(lvgl_data_struct *dat)
 
     /*Screen1: Just a picture*/
     dat->scr[0] = lv_obj_create(NULL);
+    dat->scr[1] = lv_obj_create(NULL);
+    dat->scr[2] = lv_obj_create(NULL);
     
     /*Declare and load the image resource*/
     LV_IMG_DECLARE(julian);
+    LV_IMG_DECLARE(julian2);
+    LV_IMG_DECLARE(julian3);
+
     lv_obj_t *img1 = lv_img_create(dat->scr[0]);
     lv_img_set_src(img1, &julian);
-
-    /*Align the image to the center of the screen*/
     lv_obj_align(img1, LV_ALIGN_CENTER, 0, 0);
+
+    lv_obj_t *img2 = lv_img_create(dat->scr[1]);
+    lv_img_set_src(img2, &julian2);
+    lv_obj_align(img2, LV_ALIGN_CENTER, 0, 0);
+
+    lv_obj_t *img3 = lv_img_create(dat->scr[2]);
+    lv_img_set_src(img3, &julian3);
+    lv_obj_align(img3, LV_ALIGN_CENTER, 0, 0);
 
     /*Load the first screen as the current active screen*/
     lv_scr_load(dat->scr[0]);
